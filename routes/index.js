4323001a52ee603e456da005be59b908c4d3b729
Chris Ramesh <chris@ticklerfile.com>
2018-07-02 04:47:54 +0100

    first half of google oauth working

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,41 @@
 /* Variables ==================================================================== */
 // libraries
 const routes = require('express').Router();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 // custom
 
 // constants
+const {
+  AUTH_GOOGLE_CLIENT_ID,
+  AUTH_GOOGLE_CLIENT_SECRET,
+} = process.env;
 
 /* Routes ==================================================================== */
 
 // root
 routes.get('/', (req, res) => res.status(200).send('Hello world'));
+
+// auth
+passport.use(new GoogleStrategy(
+  {
+    clientID: AUTH_GOOGLE_CLIENT_ID,
+    clientSecret: AUTH_GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback',
+  },
+  (accessToken, refreshToken, profile, done) => {
+    console.log('ACCESS token', accessToken);
+    console.log('REFRESH token', refreshToken);
+    console.log('profile', profile);
+  },
+));
+
+routes.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email'],
+}));
+
+routes.get('/auth/google/callback', passport.authenticate('google'));
 
 /* Export ==================================================================== */
 module.exports = routes;
