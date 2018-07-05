05667c7b2cec5f5e3b24d1aea9f8afb52b3fb46a
Chris Ramesh <chris@ticklerfile.com>
2018-07-05 12:58:57 +0100

    added logout route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,13 +4,21 @@ const passport = require('passport');
 
 /* Export Routes ==================================================================== */
 module.exports = (routes) => {
+  routes.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
   // google
+  // note: this route goes to google signup each time if multiple goog accounts are signed in
   routes.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email'],
   }));
-
   routes.get('/auth/google/callback', passport.authenticate('google'));
 
+  // slack
+
+  // resolved
   routes.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
